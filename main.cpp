d1b4a9d71897ef5e8061bd2ee008f9586d3dc6eb
infinity-lightning <ng05031975@gmail.com>
2022-10-13 19:31:01 +0530

    bug fix and code optimization !

--- main.cpp
+++ main.cpp
@@ -4,34 +4,41 @@ using namespace std;
 int main(){
     int h , m , s , a , err;
     err = a = 0;
-    while(err == 0){
-        cout << "Enter the time in 24-hours formate !!\n\n";
+    while(err == 0)
+    {
+        cout << "\n\nEnter the time in 24-hours formate !!\n\n";
         cout << "\nEnter hour : ";
         cin >> h;
         cout << "\nEnter minutes : ";
         cin >> m;
         cout << "\nEnter seconds : ";
         cin >> s;
-        if ( h < 24 && m<60&&s<60){
+        if ( h < 23 && m < 60 && s < 60)
+        {
             err++;
-        }else{
-            cout << "Enter the correct time !! (OwO)";
+        }
+        else
+        {
             system("cls");
         }
-        while(a == 0){
+        while(a == 0)
+        {
             system("cls");
             cout << h <<':'<< m <<':'<< s << endl;
             Sleep(1000);
             s++;
-            if(s>59){
+            if(s>59)
+            {
                 s = 0;
                 m++;
             }
-            if(m>59){
+            if(m>59)
+            {
                 m = 0;
                 h++;
             }
-            if(h>23){
+            if(h>23)
+            {
                 h=0;
             }
         }
